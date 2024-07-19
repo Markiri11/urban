c62bf003f3de3076f97deb8aca0b23b0c6243c89
Markiri11 <dfg_dsfg_dfg@mail.ru>
2024-07-19 07:28:24 +0300

    Variables

--- homework2.py
+++
@@ -1,5 +0,0 @@
-Number_of_completed_remote_sensing = 12
-Number_of_hours_spent=1.5
-Course_name="Python"
-Time_per_task=(1.5 / 12)
-print("Курс:",Course_name+",", "всего задач:",Number_of_completed_remote_sensing,"," " затрачено часов:", Number_of_hours_spent,"," " среднее время выполнения", Time_per_task,".")
